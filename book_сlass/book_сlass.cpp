3b1129982c8fa7529fc3494109c86510df555a7e
Андрей Замятин <andreizamyat1n@yandex.ru>
2024-02-13 13:26:28 +0500

    Добовление примера реализации простого класса Student

---
+++ book_сlass/book_сlass.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream> #include <string>
+using namespace std;
+
+class Book 
+{
+public: 
+	// Member Variables 
+	string title; 
+	string author; 
+	int publicationYear; 
+	float price;
+
+	// Constructor 
+	Book(string t, string a, int year, float p) : title(t), author(a), publicationYear(year), price(p) {}
+
+	// Member Function to display book details 
+	void displayDetails() 
+	{ 
+		cout << "Title: " << title << endl; 
+		cout << "Author: " << author << endl;
+		cout << "Publication Year: " << publicationYear << endl; 
+		cout << "Price: $" << price << endl;
+	}
+	// Member Function to update the book's price 
+	void updatePrice(float newPrice) 
+	{ 
+		price = newPrice; 
+	} 
+};
